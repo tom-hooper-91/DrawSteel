3de9b4b5ceeefdd427012f65d9c92009094ec558
Tom Hooper <tom.hooper@spektrix.com>
2024-12-11 11:11:16 +0000

    Refactor CreateCharacterShould

--- src/Tests/Unit/API/CreateCharacterShould.cs
+++ src/Tests/Unit/API/CreateCharacterShould.cs
@@ -9,18 +9,32 @@ namespace Tests.Unit.API;
 [TestFixture]
 public class CreateCharacterShould
 {
+    private Characters _createCharacterApi;
+    private ICreateCharacter _createCharacterAction;
+
+    [SetUp]
+    public void Setup()
+    {
+        _createCharacterAction = A.Fake<ICreateCharacter>();
+        _createCharacterApi = new Characters(_createCharacterAction);
+    }
+    
     [TestCase("Test")]
     [TestCase("TestTwo")]
     public void Call_CreateCharacter_action(string characterName)
     {
-        var createCharacterAction = A.Fake<ICreateCharacter>();
-        var createCharacterApi = new Characters(createCharacterAction);
         var command = new CreateCharacterCommand(characterName);
+        var fakeRequest = GivenApiRequestWithValidCharacterName(characterName);
+
+        _createCharacterApi.Create(fakeRequest);
+
+        A.CallTo(() => _createCharacterAction.Execute(command)).MustHaveHappened();
+    }
+
+    private static HttpRequest GivenApiRequestWithValidCharacterName(string characterName)
+    {
         var fakeRequest = A.Fake<HttpRequest>();
         fakeRequest.Body = new StringContent(JsonSerializer.Serialize(new { Name = characterName })).ReadAsStream();
-        
-        createCharacterApi.Create(fakeRequest);
-
-        A.CallTo(() => createCharacterAction.Execute(command)).MustHaveHappened();
+        return fakeRequest;
     }
 }
