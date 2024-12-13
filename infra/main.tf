a9bcf1f198747d65235dbc21077f9cb5e5f9c55d
Tom Hooper <tom.hooper@spektrix.com>
2024-12-13 19:17:50 +0000

    Extract ID's to variables

--- infra/main.tf
+++ infra/main.tf
@@ -18,11 +18,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "c91bcad2-4067-42b8-922a-59fdb7707ad6"
+  subscription_id = var.azure_subscription_id
 }
 
 provider "azuread" {
-  tenant_id = "367128d9-c2d4-4615-98a3-002700e530d3"
+  tenant_id = var.azure_tenant_id
 }
 
 provider "github" {
