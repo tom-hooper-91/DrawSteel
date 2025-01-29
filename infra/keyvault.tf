4fb9b30a48c4fce002fbeb814552eb8131dac78c
Tom Hooper <tom.hooper@spektrix.com>
2025-01-29 09:59:37 +0000

    Update access policy name

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "main" {
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "client" {
+resource "azurerm_key_vault_access_policy" "user" {
   key_vault_id = azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
