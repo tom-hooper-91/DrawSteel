661ec86aa9d1d5e354dd5dcc7812a70b115e3b46
Patrick Johnstone <patrick.johnstone@spektrix.com>
2025-03-07 08:33:30 +0000

    Add Seeder to git by changing gitignore

---
+++ src/Infrastructure/Data/CharacterSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace Infrastructure.Data;
+
+public class CharacterSeeder
+{
+    public async Task SeedDatabase(CosmosClient client)
+    {
+        const string databaseName = "drawsteel";
+        const string containerName = "characters";
+        var database = client.CreateDatabaseIfNotExistsAsync(databaseName).Result.Database;
+        var containerProperties = new ContainerProperties
+        {
+            Id = containerName
+        };
+        await database.CreateContainerAsync(containerProperties);
+    }
+}
